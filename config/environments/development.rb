cc76c267c17e37c9b3da53d15699619ffdd3857d
opuene <opusfibz@gmail.com>
2019-10-21 17:13:19 +0000

    setting up the devise gem

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -2,7 +2,6 @@ Rails.application.configure do
   config.action_mailer.default_url_options = { host: 'localhost:3030'}
 
   # a bunch of other stuff .....
-end
 
   # Settings specified here will take precedence over those in config/application.rb.
 
