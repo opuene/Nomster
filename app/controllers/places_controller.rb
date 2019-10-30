541f28fab582f2bf368026ddf6ba10ba8fb7f9e1
opuene <opusfibz@gmail.com>
2019-10-30 01:23:23 +0000

    modify

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -1,5 +1,6 @@
 class PlacesController < ApplicationController
-  before_action :authenticate_user!, only: [:new, :create]
+  before_action :authenticate_user!, only: [:new, :create, :edit, :update]
+  # A bunch of other code here
 
   def index
     @places = Place.all
@@ -20,6 +21,10 @@ class PlacesController < ApplicationController
 
   def edit
     @place = Place.find(params[:id])
+
+    if @place.user != current_user
+      return render plain: 'Not Allowed', status: :forbidden
+    end
   end
 
   def update
@@ -39,5 +44,4 @@ class PlacesController < ApplicationController
   def place_params
     params.require(:place).permit(:name, :description, :address)
   end
-
 end
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -2,7 +2,6 @@ Rails.application.configure do
   config.action_mailer.default_url_options = { host: 'nomster-opuene.herokuapp.com'}
 
   # a bunch of other stuff .....
-end
 
   # Settings specified here will take precedence over those in config/application.rb.
 
