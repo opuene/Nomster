555f10df1a2946b9a635a91bce41bf2f143451c7
opuene <opusfibz@gmail.com>
2019-11-08 02:20:52 +0000

    correcting places.controller

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -11,8 +11,8 @@ class PlacesController < ApplicationController
   end
 
   def create
-    current_user.places.create(place_params)
-    if @place.invalid?
+    @place = current_user.places.create(place_params)
+    if @place.valid?
       redirect_to root_path
     else
       render :new, status: :unprocessable_entity
